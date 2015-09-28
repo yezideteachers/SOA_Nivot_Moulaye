96e684d22dc13dd1a86cc2e7434a8ea7cdc3b148
yezideteachers <ob005696@etu.unice.fr>
2015-09-28 16:35:43 +0200

    commit
    first commit

---
+++ src/main/java/G_F/polytech/unice/fr/SOA_Nivot_Moulaye/App.java
@@ -0,0 +1,13 @@
+package G_F.polytech.unice.fr.SOA_Nivot_Moulaye;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
